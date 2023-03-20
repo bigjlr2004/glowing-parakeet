4e5c6a3e87ff5879bd1e941e266d08f4bc7063bc
John Roberts <bigjlr2004@yahoo.com>
2023-03-20 07:25:42 -0500

    Initial Commit

---
+++ DogGone/Models/Neighborhood.cs
@@ -0,0 +1,9 @@
+﻿namespace DogGone.Models
+{
+    public class Neighborhood
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ DogGone/Repositories/IWalkerRepository.cs
@@ -0,0 +1,12 @@
+﻿using DogGone.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DogGone.Repositories
+{
+    public interface IWalkerRepository
+    {
+        List<Walker> GetAllWalkers();
+        Walker GetWalkerById(int id);
+    }
+}
